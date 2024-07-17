263b84dcce1bcba95f98ec74f24e8403c4609a5c
unknown <juanito2103@hotmail.es>
2024-07-17 16:42:30 -0500

    v4

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from typing import Final
+from  telegram import Update
+from telegram.ext import Aplication ,CommandHandler,MessageHandler,filters,ContextTypes
+
 TOKEN:Final="7004412096:AAH5kltUrZmD9TCOhCr8bmbgxEfa_a-Jb9o";
 BOT_USERNAME:Final="@tensasbot";
