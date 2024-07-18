acee74a7d480f60ceccb67d383a4d0103d9f8724
unknown <juanito2103@hotmail.es>
2024-07-18 16:29:03 -0500

    v6

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ async def start_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hola como estas es mi bot de telegram hecho por python Sebas")
 
 async def start_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Soy el primer botde telegram hecho por mi creador")
+    await update.message.reply_text("Soy el primer bot de telegram hecho por mi creador")
 async def start_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Soy TensasBot ,bienvenido!")
 #Reponses
 
 def handle_reponse(text:str)->str:
-    processed:str=text.lowwer();
+    processed:str=text.lower();
     if "hola"or "Hola" in processed:
         return "Hola"
     if "como estas"or "Como estas?" in processed:
         return "estoy super bien"
     return "no puedo entenderte"
+async def handle_message(update:Update, Context:ContextTypes.DEFAULT_TYPE):
+    message_type:str= update.message.chat.type
+    text: str=update.text
+    print(f'User({update.message.chat.id}in {message_type})')
