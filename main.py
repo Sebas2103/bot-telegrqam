05d8891680a54f04d4eeca34e53a130cfeaaac14
unknown <juanito2103@hotmail.es>
2024-07-30 15:31:20 -0500

    version2

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def start_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Soy TensasBot ,bienvenido!")
 #Reponses
 
-def handle_reponse(text:str)->str:
+def handle_response(text:str)->str:
     processed:str=text.lower();
     if "hola"or "Hola" in processed:
         return "Hola"
@@ -23,5 +23,16 @@ def handle_reponse(text:str)->str:
     return "no puedo entenderte"
 async def handle_message(update:Update, Context:ContextTypes.DEFAULT_TYPE):
     message_type:str= update.message.chat.type
-    text: str=update.text
+    text: str=update.message.text
     print(f'User({update.message.chat.id}in {message_type})')
+
+    if message_type=="group":
+        if BOT_USERNAME in text:
+            new_text:str=text.replace(BOT_USERNAME,"").strip()
+            response : str= handle_response(new_text)
+        else:
+            return
+    else:
+        reponse: str= handle_response(text)
+        print('bot:',response)
+        await update.message.reply_text(response)
